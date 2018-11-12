2426c05ada0ebbb2cdbac8e01a7b0c252c56fa07
shinoka7 <shinoka7@github.com>
2018-11-11 20:06:39 -0500

    updating local

---
+++ frontend/src/components/register - Copy.vue
@@ -0,0 +1,45 @@
+<template>
+    <form action="/registered.php">
+    <div class="container">
+        <router-link to="/">Back</router-link>
+        <h2>Notification Registration</h2>
+        <div>
+            <lable for="stops">Target Stop: </label>
+            <select name="stops">
+                <option value="union">Union</option>
+                <option value="blitman">Blitman</option>
+                //and so on
+            </select>
+        </div>
+        <div>
+            <label for="route">Shuttle Route: </label>
+                <input type="text" id="route" name="route" required />
+        <div>
+            <label for="number">Phone Number: </label>
+                <input type="text" id="number"
+                name="number" required />
+        </div>
+        <div>
+            <label for="carrier">Carrier Type: </label>
+                <input type="text" id="carrier"
+                name="carrier" required /> 
+        </div>
+    </div>
+    <div>
+        <input type="submit" class="submit" value="Submit" />
+    </div>
+    </form>
+</template>
+<script lang="ts">
+// This component is the register window that is displayed when the `register` button on the menu is selected
+
+import Vue from 'vue';
+export default Vue.extend({
+
+});
+</script>
+<style lang="scss">
+    .container{
+        margin: 20px;
+    }
+</style>
